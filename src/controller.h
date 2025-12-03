5895b3dff79787519ae5d5599b9dc41403e59821
Jose Carlos Carita <87034576+josecarita@users.noreply.github.com>
2025-12-03 16:20:50 -0500

    redistribucion de clases

--- src/controller.h
+++ src/controller.h
@@ -1,4 +1,3 @@
-
 #ifndef FINAL_PROYECT_PROGRAMMING_II_CONTROLLER_H
 #define FINAL_PROYECT_PROGRAMMING_II_CONTROLLER_H
 
@@ -17,5 +16,17 @@ private:
     void process_line(const std::string &line);
 };
 
+
+struct Unit
+{
+    UnitType type;
+    Owner owner;
+    int hp;
+    int atk;
+    int def;
+    int move; // movement points (abstract)
+    Unit(UnitType t = UnitType::NONE_U, Owner o = Owner::NONE);
+    std::string to_string() const;
+};
 
 #endif //FINAL_PROYECT_PROGRAMMING_II_CONTROLLER_H
